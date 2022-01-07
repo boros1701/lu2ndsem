821af3b1a213c79f537b8d6c5a05bdef6556ca95
boros1701 <97278953+boros1701@users.noreply.github.com>
2022-01-07 09:57:06 +0200

    Create lab_G3(linked).cpp

---
+++ lab_G3(linked).cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+class LinkedList {
+public:
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void createNode(int value) {
+        Node *temp = new Node;
+        temp->data = value;
+        temp->next = nullptr;
+
+        if (head == nullptr) {
+            head = temp;
+            tail = temp;
+        } else {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    void removeLower() {
+        auto *current = head;
+        if (current == nullptr) return;
+        auto prev = current;
+        for (auto it = current->next; it != nullptr; it = it->next) {
+            if (it->data < prev->data) {
+                prev->next = it->next;
+            } else {
+                prev = it;
+            }
+        }
+    }
+
+    void printList() {
+        Node *current = head;
+        std::cout << '(';
+        while (current != nullptr) {
+            std::cout << current->data;
+            current = current->next;
+            if (current != nullptr) std::cout << ", ";
+        }
+        std::cout << ')' << std::endl;
+    }
+
+private:
+    Node *head;
+    Node *tail;
+};
+
+using namespace std;
+
+LinkedList createList(string fileName) {
+    ifstream in(fileName);
+    auto list = LinkedList();
+
+    while (!in.eof()) {
+        int n;
+        in >> n;
+        list.createNode(n);
+    }
+    in.close();
+    return list;
+}
+
+int main() {
+    auto list = createList("in.txt");
+
+    list.printList();
+    list.removeLower();
+    list.printList();
+
+    return 0;
+}
